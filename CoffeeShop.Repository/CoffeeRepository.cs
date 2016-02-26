06a57bc6b207e77c86713cd9212fdbb4d974d9ed
andreij6 <andreij6@gmail.com>
2016-02-26 01:15:32 -0500

    Setup Project Structure. Added Repository and Models

---
+++ CoffeeShop.Repository/CoffeeRepository.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CoffeeShop.Domain;
+
+namespace CoffeeShop.Repository
+{
+	public class CoffeeRepository : ICoffeeRepository
+	{
+		public CoffeeRepository()
+		{
+
+		}
+
+		public void Add(Coffee entity)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void Delete(int id)
+		{
+			throw new NotImplementedException();
+		}
+
+		public IEnumerable<Coffee> GetAll()
+		{
+			throw new NotImplementedException();
+		}
+
+		public Coffee GetById(int id)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void Update(Coffee update, int id)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
---
+++ CoffeeShop.Repository/IDataRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoffeeShop.Repository
+{
+	public interface IDataRepository<T>
+	{
+		T GetById(int id);
+
+		IEnumerable<T> GetAll();
+
+		void Update(T update, int id);
+
+		void Delete(int id);
+
+		void Add(T entity);
+	}
+}
