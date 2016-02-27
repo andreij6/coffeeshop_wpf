0e978bac35f69da5817e6164e542dc336a09c35d
andreij6 <andreij6@gmail.com>
2016-02-26 19:53:08 -0500

    added MetroWindow style

---
+++ CoffeeShop.Service/ICoffeeDataService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CoffeeShop.Domain;
+
+namespace CoffeeShop.Service
+{
+	public interface ICoffeeDataService
+	{
+		Coffee GetCoffeeDetail(int id);
+
+		IEnumerable<Coffee> GetAllCoffees();
+
+		void UpdateCoffee(Coffee coffee);
+
+		void DeleteCoffee(Coffee coffee);
+	}
+}
